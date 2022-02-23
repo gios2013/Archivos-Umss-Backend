eb50dee70e6c6090ca5a9a5ad4b74abc3d3f1550
pattyt0 <pattyopatito@gmail.com>
2022-02-22 22:04:01 -0400

    - minor change

--- aub-web-module/src/main/java/org/umss/aub/AUBApplication.java
+++ aub-web-module/src/main/java/org/umss/aub/AUBApplication.java
@@ -58,7 +58,7 @@ public class AUBApplication {
         }
 
         var textBlock = """
-
+                
                 ----------------------------------------------------------
                     Application "%s" is running! Access URLs:
                     Local:              %s://localhost:%s%s
